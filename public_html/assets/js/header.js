1c6d42285755fa8a5cd422376c0290930b833cfc
Giacomo Longo <gabibbo97@gmail.com>
2018-12-16 12:14:05 +0100

    Aggiunto auto-testing per javascript

--- public_html/assets/js/header.js
+++ public_html/assets/js/header.js
@@ -1,16 +1,28 @@
-function headerOpenHamburgerMenu() {
+function headerOpenHamburgerMenu () {
+
+  "use strict";
   // Prendi navbar-menu
-  let navbar = document.getElementsByClassName("navbar-menu")[0];
+  const navbar = document.getElementsByClassName("navbar-menu")[0];
   // Prendi navbar-burger
-  let burger = document.getElementsByClassName("navbar-burger")[0];
+  const burger = document.getElementsByClassName("navbar-burger")[0];
+
   // Funzione che aggiunge/toglie is-active
-  function toggleActive(element) {
-    if (element.classList.contains("is-active"))
+  function toggleActive (element) {
+
+    if (element.classList.contains("is-active")) {
+
       element.classList.remove("is-active");
-    else
+
+    } else {
+
       element.classList.add("is-active");
+
+    }
+
   }
+
   // Invoca la funzione sul menú e sul pulsante
   toggleActive(navbar);
   toggleActive(burger);
+
 }
