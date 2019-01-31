1968e72a1207ff906d8ad91fee9059a0405a41b3
Giacomo Longo <gabibbo97@gmail.com>
2019-01-31 11:47:44 +0100

    Risolti gli errori del linter JavaScript
    - A quanto pare per passare una funzione a un'altra, esiste una nuova sintassi che rende esplicita questa pratica, le "arrow functions", invece di function(){} si mette () => {}
    - Aggiunto lo use-strict
    - Il form lo generiamo a parte
    - Buona parte degli elementi ora é costante, nel senso che la variabile non viene piú aggiornata dopo la definizione, secondo ESLint i browser possono fare delle assunzioni su come funziona il codice e dare meno problemi
    - Aggiunti molti spazi e linebreak

--- public_html/assets/js/gdpr.js
+++ public_html/assets/js/gdpr.js
@@ -1,44 +1,76 @@
-document.addEventListener("DOMContentLoaded", function() {
-  function displayGDPR() {
-    // Genera l'elemento che oscura la pagina
-    let sfondo = document.createElement("aside");
-    sfondo.id = "GDPR";
+document.addEventListener("DOMContentLoaded", () => {
+
+  "use strict";
+  function generateGDPRForm () {
+
     // Genera il form GDPR
-    let form = document.createElement("form");
-    let formElement1 = document.createElement("span");
+    const form = document.createElement("form");
+
+    // Scritta
+    const formElement1 = document.createElement("span");
     formElement1.textContent = "Nonostante i biscotti non siano indicati per l'alimentazione delle papere, lei, in quanto umano, é tenuto ad acconsentire a ricevere nel suo navigatore dei freschissimi cookie";
-    let formElement2 = document.createElement("button");
+
+    // Pulsante
+    const formElement2 = document.createElement("button");
     formElement2.textContent = "Gnam cookies";
-    let formElement3 = document.createElement("href");
+
+    // Link ai termini e condizioni
+    const formElement3 = document.createElement("href");
     formElement3.textContent = "La ricetta dei nostri cookies";
     formElement3.href = "https://ricette.giallozafferano.it/Cookies.html";
+
     // Aggiungi i figli al form
     form.appendChild(formElement1);
     form.appendChild(formElement2);
     form.appendChild(formElement3);
+
+    // Ritorna il form
+    return form;
+
+  }
+
+  function displayGDPR () {
+
+    // Genera l'elemento che oscura la pagina
+    const sfondo = document.createElement("aside");
+    sfondo.id = "GDPR";
+
+    // Genera il form GDPR
+    const form = generateGDPRForm();
+
     // Aggiungi il form allo sfondo
     sfondo.appendChild(form);
+
     // Aggiungi lo sfondo alla pagina
     document.body.appendChild(sfondo);
+
     // Visualizza l'elemento
     const gdpr = document.getElementById("GDPR");
-    gdpr.style.display = 'flex';
-    gdpr.children[0].children[1].onclick = function() {
-      let now = new Date();
-      document.cookie = "gdprconsent=" + encodeURIComponent(now.toString()) + ";max-age=31536000";
-    }
+    gdpr.style.display = "flex";
+    gdpr.children[0].children[1].onclick = () => {
+
+      const now = new Date();
+      document.cookie = `gdprconsent=${encodeURIComponent(now.toString())};max-age=31536000`;
+
+    };
+
   }
 
 
-  if (! document.cookie.includes("gdprconsent")) {
+  if (!document.cookie.includes("gdprconsent")) {
+
     // Genera il link al CSS per il form
-    let cssLink = document.createElement("link");
+    const cssLink = document.createElement("link");
     cssLink.type = "text/css";
     cssLink.rel = "stylesheet";
     cssLink.href = "assets/css/gdpr.css";
-    cssLink.onload = function() {
-      displayGDPR()
-    }
+    cssLink.onload = () => {
+
+      displayGDPR();
+
+    };
     document.getElementsByTagName("head")[0].appendChild(cssLink);
+
   }
+
 });
