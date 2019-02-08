0592abac5c1983aec7f10a8cc7429a1dcd05b212
Giacomo Longo <gabibbo97@gmail.com>
2019-02-08 10:56:04 +0100

    Aggiunto mostra password

--- public_html/assets/js/header.js
+++ public_html/assets/js/header.js
@@ -62,3 +62,32 @@ function headerOpenLoginBar () {
   }
 
 }
+
+function showPassword(button) {
+
+  // Prendi l'elemento della password
+  const password = document.getElementById("loginPassword");
+
+  // Prendi l'icona
+  const icon = button.children[0].children[0];
+
+  // Cambia il tipo dell'input
+  if (password.type === "password") {
+
+    // Imposta il testo su visibile
+    password.type = "text";
+
+    // Cambia icona
+    icon.classList.toggle("fa-eye-slash");
+
+  } else {
+
+    // Imposta il testo su nascosto
+    password.type = "password";
+
+    // Cambia icona
+    icon.classList.toggle("fa-eye");
+
+  }
+
+}
