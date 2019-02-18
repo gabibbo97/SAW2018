f97b7305cb8b57bb03f78d27ca38241516fcb3de
Giacomo Longo <gabibbo97@gmail.com>
2019-02-18 14:55:05 +0100

    Utilizzata nuova pagina

--- html/index.php
+++ html/index.php
@@ -1,45 +1,110 @@
 <?php
 require '../lib/head.php';
-drawHead("Homepage", "La nostra homepage");
+drawHead("Homepage", "La nostra homepage", array(
+    '<link rel="stylesheet" href=" https://unpkg.com/bulma-carousel@3.0.0/dist/css/bulma-carousel.min.css" />',
+    '<script async defer src="https://unpkg.com/bulma-carousel@3.0.0/dist/js/bulma-carousel.min.js"></script>',
+    '<script>document.addEventListener("DOMContentLoaded", () => { var carousels = bulmaCarousel.attach();  })</script>',
+));
 ?>
 
 <body>
-  <?php require '../lib/header.php';?>
-  <main class="section">
-    <section class="box hero is-warning is-bold">
-      <div class="hero-body">
-        <div class="container">
-          <h1 class="title is-size-1">Le mille piú uno paperelle</h1>
-          <h2 class="subtitle is-size-3">Scegli la tua preferita tra una ricca collezione di papere selezionate dai
-            migliori esperti del globo in pennuti in vinile.</h2>
+    <?php require '../lib/header.php';?>
+    <main class="section">
+        <div class="tile is-ancestor">
+            <div class="tile is-4 is-vertical is-parent">
+
+                <article class=" is-child message is-link">
+                    <div class="message-body">
+                        <p class="title">Le mille piú uno paperelle</p>
+                        <p>Scegli la tua paperella preferita tra una ricca collezione di papere selezionate dai
+                            migliori esperti del globo in pennuti in vinile. <br> Visita io nostro catalogo!</p>
+                    </div>
+                </article>
+
+                <article class=" is-child message is-warning">
+                    <div class="message-body">
+                        <p class="title">Un prodotto pregiato</p>
+                        <div class="content ">
+                            <ul>
+                                <li>Tutte le nostre papere sono certificate CE (Comunitá Europea).</li>
+                                <li>Nessun materiale tossico o nocivo.</li>
+                                <li>Severo controllo di qualitá (ciascuna papera viene sottoposta a un bagno di
+                                    prova da uno dei
+                                    nostri addetti).</li>
+                                <li>Tutti i nostri impiegati sono contenti di lavorare per noi.</li>
+                            </ul>
+                        </div>
+                    </div>
+                </article>
+
+                <article class=" is-child message is-success is-hidden-touch">
+                    <div class="message-body">
+                        <p class="title">Collezionale tutte</p>
+                        <div class="content ">
+                            <ul>
+                                <li>Originali</li>
+                                <li>Divertenti</li>
+                                <li>Semplici</li>
+                                <li>Uniche</li>
+                            </ul>
+                        </div>
+                        <p>Vieni a trovarci, ti aspettiamo!</p>
+                    </div>
+                </article>
+
+            </div>
+            <div class="tile is-8 is-vertical is-parent">
+                <div class='is-child carousel carousel-animated carousel-animate-slide' data-autoplay="true;"
+                    style="max-height: 50em;">
+                    <div class='carousel-container'>
+                        <div class='carousel-item has-background is-active'>
+                            <img class="is-background" src="assets/images/carousel-1.jpg" alt="" width="640"
+                                height="310" />
+                        </div>
+                        <div class='carousel-item has-background'>
+                            <img class="is-background" src="assets/images/carousel-2.jpg" alt="" width="640"
+                                height="310" />
+
+                        </div>
+                        <div class='carousel-item has-background'>
+                            <img class="is-background" src="assets/images/register.jpg" alt="" width="640"
+                                height="310" />
+
+                        </div>
+                        <div class='carousel-item has-background'>
+                            <img class="is-background" src="assets/images/home.jpg" alt="" width="640" height="310" />
+
+                        </div>
+
+                    </div>
+                    <div class="carousel-navigation is-overlay">
+                        <div class="carousel-nav-left">
+                            <i class="fa fa-chevron-left"></i>
+                        </div>
+                        <div class="carousel-nav-right">
+                            <i class="fa fa-chevron-right"></i>
+                        </div>
+                    </div>
+                </div>
+                <hr class="is-hidden-desktop">
+                <div class="tile is-child message is-success is-hidden-desktop">
+                    <div class="message-body">
+                        <p class="title">Collezionale tutte</p>
+                        <div class="content" style="color: rgb(7, 85, 81);">
+                            <ul>
+                                <li>Originali</li>
+                                <li>Semplici</li>
+                                <li>Uniche</li>
+                            </ul>
+                        </div>
+                        <p>Vieni a trovarci, ti aspettiamo!</p>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </section>
-    <div class="box">
-      <div class="container">
-        <figure class="image is-16by9">
-          <img style="filter: drop-shadow(0.0em 0.0em 1.0em black);" alt="Tre papere si godono un bagno rilassante" src="assets/images/home.jpg">
-        </figure>
-      </div>
-    </div>
-    <section class="box hero is-primary">
-      <div class="hero-body">
-        <div class="container">
-          <h1 class="title">Un prodotto pregiato</h1>
-          <div class="content is-size-3">
-            <ul>
-              <li>Tutte le nostre papere sono certificate CE (Comunitá Europea).</li>
-              <li>Nessun materiale tossico o nocivo.</li>
-              <li>Severo controllo di qualitá (ciascuna papera viene sottoposta a un bagno di prova da uno dei
-                nostri addetti).</li>
-              <li>Tutti i nostri impiegati sono contenti di lavorare per noi.</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </section>
-  </main>
-  <?php require '../lib/footer.php';?>
+
+    </main>
+    <?php require '../lib/footer.php';?>
 </body>
 
 </html>
