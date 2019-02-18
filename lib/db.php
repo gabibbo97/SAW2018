2915f9964c99a5b6d27fd640c83a02797f6cc8d1
Giacomo Longo <gabibbo97@gmail.com>
2019-02-18 20:05:29 +0100

    Tolto il TODO

--- lib/db.php
+++ lib/db.php
@@ -35,7 +35,7 @@
     $initializedFile = dirname($_SERVER['SCRIPT_FILENAME']).'/db-initialized';
     if (file_exists($initializedFile))
       return;
-    // TODO: RIMETTERE touch ($initializedFile);
+    touch ($initializedFile);
 
     require('config.php'); // Includi le impostazioni di configurazione
 
