fb399181efe68df4d03b5c6a9a9f6078bfc5f999
Giacomo Longo <gabibbo97@gmail.com>
2019-02-18 12:27:19 +0100

    Test nuovo indice

--- html/index-alt.php
+++ html/index-alt.php
@@ -10,53 +10,52 @@ drawHead("Homepage", "La nostra homepage", array(
 <body>
     <?php require '../lib/header.php';?>
     <main class="section">
-        <div class="box">
-            <div class="tile is-ancestor">
-                <div class="tile is-4 is-vertical is-parent">
+        <div class="tile is-ancestor">
+            <div class="tile is-4 is-vertical is-parent">
 
-                    <article class=" is-child message is-link">
-                        <div class="message-body">
-                            <p class="title">Le mille piú uno paperelle</p>
-                            <p>Scegli la tua paperella preferita tra una ricca collezione di papere selezionate dai
-                                migliori esperti del globo in pennuti in vinile. <br> Visita io nostro catalogo!</p>
+                <article class=" is-child message is-link">
+                    <div class="message-body">
+                        <p class="title">Le mille piú uno paperelle</p>
+                        <p>Scegli la tua paperella preferita tra una ricca collezione di papere selezionate dai
+                            migliori esperti del globo in pennuti in vinile. <br> Visita io nostro catalogo!</p>
+                    </div>
+                </article>
+
+                <article class=" is-child message is-warning">
+                    <div class="message-body">
+                        <p class="title">Un prodotto pregiato</p>
+                        <div class="content ">
+                            <ul>
+                                <li>Tutte le nostre papere sono certificate CE (Comunitá Europea).</li>
+                                <li>Nessun materiale tossico o nocivo.</li>
+                                <li>Severo controllo di qualitá (ciascuna papera viene sottoposta a un bagno di
+                                    prova da uno dei
+                                    nostri addetti).</li>
+                                <li>Tutti i nostri impiegati sono contenti di lavorare per noi.</li>
+                            </ul>
                         </div>
-                    </article>
+                    </div>
+                </article>
 
-                    <article class=" is-child message is-warning">
-                        <div class="message-body">
-                            <p class="title">Un prodotto pregiato</p>
-                            <div class="content ">
-                                <ul>
-                                    <li>Tutte le nostre papere sono certificate CE (Comunitá Europea).</li>
-                                    <li>Nessun materiale tossico o nocivo.</li>
-                                    <li>Severo controllo di qualitá (ciascuna papera viene sottoposta a un bagno di
-                                        prova da uno dei
-                                        nostri addetti).</li>
-                                    <li>Tutti i nostri impiegati sono contenti di lavorare per noi.</li>
-                                </ul>
-                            </div>
+                <article class=" is-child message is-success is-hidden-touch">
+                    <div class="message-body">
+                        <p class="title">Collezionale tutte</p>
+                        <div class="content ">
+                            <ul>
+                                <li>Originali</li>
+                                <li>Divertenti</li>
+                                <li>Semplici</li>
+                                <li>Uniche</li>
+                            </ul>
                         </div>
-                    </article>
+                        <p>Vieni a trovarci, ti aspettiamo!</p>
+                    </div>
+                </article>
 
-                    <article class=" is-child message is-success is-hidden-touch">
-                        <div class="message-body">
-                            <p class="title">Collezionale tutte</p>
-                            <div class="content ">
-                                <ul>
-                                    <li>Originali</li>
-                                    <li>Divertenti</li>
-                                    <li>Semplici</li>
-                                    <li>Uniche</li>
-                                </ul>
-                            </div>
-                            <p>Vieni a trovarci, ti aspettiamo!</p>
-                        </div>
-                    </article>
-
-                </div>
-
+            </div>
+            <div class="tile is-8 is-vertical is-parent">
                 <div class='is-child carousel carousel-animated carousel-animate-slide' data-autoplay="true;"
-                    style="max-height: 800px;;">
+                    style="max-height: 60em;">
                     <div class='carousel-container'>
                         <div class='carousel-item has-background is-active'>
                             <img class="is-background" src="assets/images/carousel-1.jpg" alt="" width="640"
@@ -86,22 +85,20 @@ drawHead("Homepage", "La nostra homepage", array(
                             <i class="fa fa-chevron-right"></i>
                         </div>
                     </div>
-
-                    <br>
-                    <br>
-                    <article class="is-child message is-success is-hidden-desktop">
-                        <div class="message-body">
-                            <p class="title">Collezionale tutte</p>
-                            <div class="content" style="color: rgb(7, 85, 81);">
-                                <ul>
-                                    <li>Originali</li>
-                                    <li>Semplici</li>
-                                    <li>Uniche</li>
-                                </ul>
-                            </div>
-                            <p>Vieni a trovarci, ti aspettiamo!</p>
+                </div>
+                <hr class="is-hidden-desktop">
+                <div class="tile is-child message is-success is-hidden-desktop">
+                    <div class="message-body">
+                        <p class="title">Collezionale tutte</p>
+                        <div class="content" style="color: rgb(7, 85, 81);">
+                            <ul>
+                                <li>Originali</li>
+                                <li>Semplici</li>
+                                <li>Uniche</li>
+                            </ul>
                         </div>
-                    </article>
+                        <p>Vieni a trovarci, ti aspettiamo!</p>
+                    </div>
                 </div>
             </div>
         </div>
